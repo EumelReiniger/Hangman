63b4a723cd8677742e0940b9971bf41616908338
juerwein <juerwein04@bbsandernach.de>
2024-06-13 11:14:16 +0200

    progress 13.06

--- Hangman/Application.py
+++ Hangman/Application.py
@@ -1,13 +1,28 @@
 from tkinter import *
+from tkinter import font as tkFont
 import os
 
 class Application(Frame):
     def __init__(self, master = None):
         Frame.__init__(self, master)
-        Button(master, text='O  Beenden', width=10, command = self.action).grid(
-            row = 3, column = 0, sticky = W, pady = 4
-        )
+
+        for i in range(3):
+            Frame.columnconfigure(self, index = i, weight=1)
+
+        Frame.rowconfigure(self, index = 1, weight=1)
+        font = tkFont.Font(family='Helvetica', size=24, weight=tkFont.BOLD)
+        
+        Label(Frame, text='Top left').grid(row=0, column=0, sticky='w')
+        Label(Frame, text='Top center').grid(row=0, column=1)
+        Label(Frame, text='Top right').grid(row=0, column=2, sticky='e')
+        Label(Frame, text='Center').grid(row=1, column=1)
+        # Button(master, text='Beenden', font=font, command = self.closeGame).grid(
+        #     row=0, column=0)
+
+        # Button(master, text='vs. Computer', font=font, command = self.closeGame).grid(
+        #     row=1, column=2
+        # )
     pass
 
-    def action(self):
+    def closeGame(self):
         os._exit(0)
--- Hangman/main.py
+++ Hangman/main.py
@@ -2,5 +2,8 @@ from Application import Application
 from tkinter import *
 
 root = Tk()
+root.pack_propagate(0)
+root.geometry("800x800")
+root.resizable(0, 0)
 app = Application(master = root)
 app.mainloop()
